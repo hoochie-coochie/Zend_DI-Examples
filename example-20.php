d9cc51bc3c7c72f48b53140fcb0ef8668bbe08bf
Alex Pogodin <alex@zingan.com>
2012-01-24 16:03:51 +0200

    Example 20: abstract ancestor injection

---
+++ example-20.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace MovieApp {
+    abstract class AbstractLister {
+        public $finder;
+        public function __construct(Finder $finder){
+            $this->finder = $finder;
+        }
+    }
+
+    class Lister extends AbstractLister{}
+
+    class Finder {
+        public $criteria;
+        public function __construct($criteria){
+            $this->criteria = $criteria;
+        }
+    }
+}
+
+namespace {
+    // bootstrap
+    include 'zf2bootstrap' . ((stream_resolve_include_path('zf2bootstrap.php')) ? '.php' : '.dist.php');
+
+    $di = new Zend\Di\Di;
+
+    /* PLEASE READ!
+     *
+     * The scope of this code is to use configured finder (by title in this case)
+     * in all listers extended from AbstractLister
+     * 
+     */
+    $config = new Zend\Di\Configuration(array(
+        'instance' => array(
+            'alias' => array(
+                'title-finder' => 'MovieApp\Finder',
+            ),
+            'MovieApp\AbstractLister' => array(
+                'parameters' => array(
+                    // Use configured finder
+                    'finder' => 'title-finder'
+                ),
+            ),
+            'title-finder' => array(
+                'parameters' => array(
+                    'criteria' => 'title'
+                ),
+            ),
+        ),
+        'definition' => array(
+            'class' => array(
+                'MovieApp\AbstractLister' => array(
+                    'parameters' => array(
+                        'criteria' => array('required' => true),
+                    ),
+                ),
+            ),
+        ),
+    ));
+
+    $di->configure($config);
+    $lister = $di->get('MovieApp\Lister');
+    
+    // expression to test
+    $works = ($lister->finder instanceof MovieApp\Finder);
+
+    // display result
+    echo (($works) ? 'It works!' : 'It DOES NOT work!');
+}
